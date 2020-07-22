8eecca9698e6fb9c76691e66e92589b8a603e189
kartikchitade <68079408+kartikchitade@users.noreply.github.com>
2020-07-22 23:00:53 +0530

    Add files via upload

---
+++ userRoute.js
@@ -0,0 +1,39 @@
+import express from "express";
+import User from "../models/userModels";
+
+const router = express.Router();
+
+router.post('/signin', async (req, res) => {
+  const signinUser = await User.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  if (signinUser) {
+    res.send({
+      _id: signinUser.id,
+      name: signinUser.name,
+      email: signinUser.email,
+      isAdmin: signinUser.isAdmin,
+      token: getToken(signinUser),
+    });
+  } else {
+    res.status(401).send({ message: 'Invalid Email or Password.' });
+  }
+});
+
+router.get('/createadmin', async (req, res) => {
+    try {
+      const user = new User({
+        name: 'kartik',
+        email: 'kartikchitade@gmail.com',
+        password: '1234',
+        isAdmin: true,
+      });
+      const newUser = await user.save();
+      res.send(newUser);
+    } catch (error) {
+      res.send({ message: error.message });
+    }
+  });
+  
+  export default router;
